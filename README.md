c5e80db23e0ddec2673d8ffacc24462af375711e
Anten Skrabec <cebarks@gmail.com>
2021-03-26 02:49:10 -0600

    add RuntimeSpriteSheet

--- README.md
+++ README.md
@@ -17,29 +17,40 @@ import "github.com/cebarks/spriteplus"
 ```
 
 ```golang
+//
 sheet := spriteplus.NewBasicSheet(2, 2, 4, 4, "4px-2x2-small.png")
+sheet := spriteplus.NewCachedSheet(2, 2, 4, 4, "4px-2x2-small.png")
 
-//These can be directly drawn to a pixel.Target
+//These can be directly drawn to a Window
 sprite1 := sheet.GetSprite(0) //bottom-left
 sprite2 := sheet.GetSprite(1) //bottom-right
 sprite3 := sheet.GetSprite(2) //top-left
 sprite4 := sheet.GetSprite(3) //top-right
 
-// or you can draw them to a batch using sheet.SourcePic()
+// or you can draw them to a batch (or any pixel.Target) using sheet.SourcePic()
 pic := sheet.SourcePic()
 batch := pixel.NewBatch(&pixel.TrianglesData{}, pic)
 
 sprite1.Draw(batch, pixel.IM)
+sprite2.Draw(batch, pixel.IM)
+sprite3.Draw(batch, pixel.IM)
+sprite4.Draw(batch, pixel.IM)
+
+
+batch.Draw(...
 ```
 
 ### Sheet Types
-
 #### `BasicSpriteSheet`
 This is the most basic implementation of a sprite sheet. It has not bells or whistles.
 The sprites on the sheet must all be square, of the same size, and aligned neatly to a grid.
 
 #### `CachedSpriteSheet`
 This is the same as a `BasicSpriteSheet` except the `GetSprite()` method uses a map to cache the `*pixel.Sprite` that are created instead of a new one being created every time the method is called.
+
+### Todo Sheet Types
+#### `RuntimeSpriteSheet`
+Uses [pixelutils](https://github.com/dusk125/pixelutils)
 
 ---
 
--- spritesheet.go
+++ spritesheet.go
@@ -2,6 +2,7 @@ package spriteplus
 
 import (
 	"github.com/dusk125/pixelutils"
+	"github.com/dusk125/pixelutils/packer"
 	"github.com/faiface/pixel"
 )
 
@@ -84,7 +85,7 @@ type CachedSpriteSheet struct {
 }
 
 //GetSprite will return the sprite in the Cache (or create&add it to the Cache) from the given int id
-func (css CachedSpriteSheet) GetSprite(id interface{}) *pixel.Sprite {
+func (css *CachedSpriteSheet) GetSprite(id interface{}) *pixel.Sprite {
 	sprite := css.Cache[id]
 
 	if sprite == nil {
@@ -114,6 +115,38 @@ func MakeCachedSheetFromPicture(countX, countY, sizeX, sizeY int, pic pixel.Pict
 			countY:    countY,
 		},
 		Cache: make(map[interface{}]*pixel.Sprite),
+	}
+
+	return &sheet, nil
+}
+
+type RuntimeSpriteSheet struct {
+	packr *packer.Packer
+}
+
+func (rss *RuntimeSpriteSheet) SourcePic() pixel.Picture {
+	return rss.packr.Picture()
+}
+
+//GetSprite will return the sprite in the Cache (or create&add it to the Cache) from the given int id
+func (rss *RuntimeSpriteSheet) GetSprite(id interface{}) *pixel.Sprite {
+	rss.packr.SpriteFrom(id.(int))
+	return nil
+}
+
+//BuildRuntimeSpriteSheet will build a spritesheet from the supplied []*pixel.PictureData. The ids will be assigned in order
+func BuildRuntimeSpriteSheet(pics []*pixel.PictureData) (SpriteSheet, error) {
+	packr := packer.NewPacker(0, 0, packer.OptimizeOnInsert|packer.AllowGrowth)
+
+	for _, pic := range pics { //TODO make optionally concurrent
+		err := packr.InsertPictureData(packr.GenerateId(), pic) //TODO waiting on https://github.com/dusk125/pixelutils/pull/1
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	sheet := &RuntimeSpriteSheet{
+		packr: packr,
 	}
 
 	return sheet, nil
