07fe45de828b5b9aab61ad4bd5665aeba4699876
Anten Skrabec <cebarks@gmail.com>
2021-06-17 23:25:08 -0600

    export Packer for easier debugging

--- spritesheet.go
+++ spritesheet.go
@@ -20,7 +20,7 @@ func NewSpriteSheet(allowGrowth bool, debugDraw bool) *SpriteSheet {
 	}
 
 	return &SpriteSheet{
-		packr: packer.NewPacker(256, 256, flags),
+		Packr: packer.NewPacker(256, 256, flags),
 		Alias: make(map[string]int),
 		Cache: make(map[string]*pixel.Sprite),
 	}
@@ -29,17 +29,17 @@ func NewSpriteSheet(allowGrowth bool, debugDraw bool) *SpriteSheet {
 type SpriteSheet struct {
 	Cache map[string]*pixel.Sprite
 	Alias map[string]int
-	packr *packer.Packer
+	Packr *packer.Packer
 }
 
 func (ss *SpriteSheet) AddSprite(pic pixel.Picture, id string) error {
-	intId := ss.packr.GenerateId()
+	intId := ss.Packr.GenerateId()
 
 	ss.Alias[id] = intId
 
-	err := ss.packr.InsertPictureDataV(intId, pic.(*pixel.PictureData), packer.OptimizeOnInsert)
+	err := ss.Packr.InsertPictureDataV(intId, pic.(*pixel.PictureData), packer.OptimizeOnInsert)
 
-	ss.Cache[id] = ss.packr.SpriteFrom(ss.Alias[id])
+	ss.Cache[id] = ss.Packr.SpriteFrom(ss.Alias[id])
 
 	if err != nil {
 		return err
@@ -88,7 +88,7 @@ func (ss *SpriteSheet) GetSprite(id string) *pixel.Sprite {
 	sprite := ss.Cache[id]
 
 	if sprite == nil {
-		sprite = ss.packr.SpriteFrom(ss.Alias[id])
+		sprite = ss.Packr.SpriteFrom(ss.Alias[id])
 		ss.Cache[id] = sprite
 	}
 
@@ -97,5 +97,5 @@ func (ss *SpriteSheet) GetSprite(id string) *pixel.Sprite {
 
 //SourcePic returns the underlying pixel.Picture of the spritesheet
 func (ss SpriteSheet) SourcePic() pixel.Picture {
-	return ss.packr.Picture()
+	return ss.Packr.Picture()
 }
