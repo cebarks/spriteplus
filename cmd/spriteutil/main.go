33e5652ae37cec2d2711dbfe4e0927d96b753df3
Anten Skrabec <cebarks@gmail.com>
2021-08-15 16:58:30 -0600

    fix util program spliting

--- cmd/spriteutil/main.go
+++ cmd/spriteutil/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
-	"image"
+	"fmt"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,34 +18,46 @@ func main() {
 	height := flag.Int("height", 16, "height of each sprite")
 	width := flag.Int("width", 16, "width of each sprite")
 
+	flag.Parse()
+
 	pwd, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("couldn't get working directory: %v", err)
 	}
 
 	err = os.Mkdir(filepath.Join(pwd, "out"), 0744)
-	if err != nil {
-		log.Fatalf("couldn't make output directory: %v", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("couldn't make output directory: %T: %s", err, err)
+	// }
 
-	file, err := os.Open(*in)
+	infile, err := os.Open(filepath.Join(pwd, *in))
 	if err != nil {
-		log.Fatalf("Couldn't make output directory: %v", err)
+		log.Fatalf("Couldn't open input file: %v", err)
 	}
-	defer file.Close()
+	defer infile.Close()
 
-	img, _, err := image.Decode(file)
+	img, err := png.Decode(infile)
 	if err != nil {
-		log.Fatalf("Couldn't make output directory: %v", err)
+		log.Fatalf("Couldn't decode input file: %v", err)
 	}
 
 	for x := 0; x < img.Bounds().Max.X / *width; x++ {
 		for y := 0; y < img.Bounds().Max.Y / *height; y++ {
-			sub, err := spriteplus.Subimage(img)
+			sub, err := spriteplus.Subimage(img, x**width, y**height, x**width+*width, y**height+*height)
 			if err != nil {
-				return nil, err
+				log.Fatalln(err)
+			}
+
+			outfile, err := os.Create(filepath.Join(pwd, "out", fmt.Sprintf("%v-%v.png", x, y)))
+			defer outfile.Close()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			err = png.Encode(outfile, sub)
+			if err != nil {
+				log.Fatalln(err)
 			}
 		}
 	}
-
 }
